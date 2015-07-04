303b9f41a8e264246ef2bc688af851b0100c0b23
Eswar <kvn.eswar@hotmail.com>
2015-07-04 15:31:06 -0600

    Initial Commit for 	JAX-WS RPC style WS

---
+++ JAX-WS-RPC-HelloWorldStandalone/src/src/org/webservice/jaxws/client/Client.java
@@ -0,0 +1,21 @@
+package org.webservice.jaxws.client;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import org.webservice.jaxws.server.HelloWorld;
+
+public class Client {
+
+	public static void main(String[] args) throws MalformedURLException {
+		URL url = new URL("http://localhost:9999/ws/hello?wsdl");
+		QName qName = new QName("http://impl.server.jaxws.webservice.org/", "HelloWorldImplService");
+		Service service = Service.create(url, qName);
+		HelloWorld helloWorld = service.getPort(HelloWorld.class);
+		System.out.println(helloWorld.sayHello("Eswar"));
+	}
+
+}
---
+++ JAX-WS-RPC-HelloWorldStandalone/src/src/org/webservice/jaxws/server/impl/HelloWorldImpl.java
@@ -0,0 +1,16 @@
+package org.webservice.jaxws.server.impl;
+
+import javax.jws.WebService;
+
+import org.webservice.jaxws.server.HelloWorld;
+
+@WebService(endpointInterface="org.webservice.jaxws.server.HelloWorld")
+public class HelloWorldImpl implements HelloWorld {
+
+	@Override
+	public String sayHello(String name) {
+		return "Hello "+name;
+	}
+
+	
+}
